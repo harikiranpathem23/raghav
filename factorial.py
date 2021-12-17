a7f7dd6dd5d4b10e3f48009b7bcced22d16048ab
harikirannnn <xyz@gmail.com>
2021-12-17 09:47:53 +0530

    fibonacci programm added-2nd commit

--- factorial.py
+++ factorial.py
@@ -1,11 +1,12 @@
 
-# import math
-# print(math.factorial(5))
+import math
+print(math.factorial(5))
 
-# x= int(input('enter a number : '))
-# sum=1
-# while x>1:
-#     sum=sum*x
-#     x-=1
-# print(sum)
+x= int(input('enter a number : '))
+sum=1
+while x>1:
+    sum=sum*x
+    x-=1
+print(sum)
+print(5)
 
---
+++ fibonacci series.py
@@ -0,0 +1,23 @@
+# x=int(input('enter a number: '))
+# count=0
+# a=0
+# b=1
+# while count<x:
+#     print(a,end=' ')
+#     n=a+b
+#     a=b
+#     b=n
+#     count+=1
+#
+# x=int(input('enter a number: '))
+# count=0
+# a=0
+# b=1
+# while count<x:
+#     print(a, end=' ')
+#     n=a+b
+#     a=b
+#     b=n
+#     count+=1
+
+
